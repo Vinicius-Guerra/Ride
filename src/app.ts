5f603c2eb3843b58505ef79e75e1e927d8395f01
Vinicius Guerra <vini.fms@hotmail.com>
2024-05-10 09:59:42 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import "express-async-errors";
+
+
+const app = express();
+
+export const initApp = () => {
+    app.use(express.json());
+
+};
+
+export default app;
