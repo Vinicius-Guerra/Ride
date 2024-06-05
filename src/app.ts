8a932d59e394eb7bdd1640b832b79ab908d095d3
Vinicius Guerra <vini.fms@hotmail.com>
2024-06-05 17:52:49 -0300

    Cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 import { initSwagger } from "./@shared/configs/swagger";
 import { initRoutes } from "./routes";
 import { initLoggers } from "./@shared/loggers";
@@ -8,6 +9,7 @@ import { initErrorHandler } from "./@shared/errors";
 export const initApp = () => {
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
   initLoggers(app);
   initRoutes(app);
